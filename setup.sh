10e95bbf0c0c988536b9a31dee7f1e9c41692e2b
Ben Hardill <hardillb@gmail.com>
2020-12-24 13:56:43 +0000

    Fix quoting in setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ else
 	reg=""
 fi
 
-sed -i -e 's/private.example.com/$reg/g' deployment/deployment.yml
-sed -i -e 's/example.com/$rd/g' deployment/deployment.yml
-sed -i -e 's/example.com/$rd/g' deployment/ingress.yml
-sed -i -e 's/example.com/$rd/g' custom-node-red/settings.js
+sed -i -e "s/private.example.com/$reg/g" deployment/deployment.yml
+sed -i -e "s/example.com/$rd/g" deployment/deployment.yml
+sed -i -e "s/example.com/$rd/g" deployment/ingress.yml
+sed -i -e "s/example.com/$rd/g" custom-node-red/settings.js
